c491277fb41bca9adaac06601b40d38108600c4c
Irfanbd <gourob83@gmail.com>
2021-10-04 13:24:11 +0600

    Beautify CODE

--- add.php
+++ add.php
@@ -2,8 +2,8 @@
 <div id="main-content">
     <h2>Add New Record</h2>
     <?php
-    $conn = mysqli_connect("localhost","root","","crud")
-    ?>
+$conn = mysqli_connect("localhost", "root", "", "crud")
+?>
     <form class="post-form" action="savedata.php" method="post">
         <div class="form-group">
             <label>Name</label>
@@ -18,21 +18,15 @@
             <select name="sclass">
                 <option value="" selected disabled>Select Class</option>
                 <?php
-                $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed");
-
-                $sql = "SELECT * FROM studentclass ";
-                
-               $result = mysqli_query($conn,$sql) or die("Unsuccessful Query");
-
-
-            while($row =mysqli_fetch_assoc($result))
-            {
-
-                ?>
-                <option value="<?php  echo $row['cid']; ?>"><?php  echo $row['cname']; ?></option>
+$conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
+$sql = "SELECT * FROM studentclass ";
+$result = mysqli_query($conn, $sql) or die("Unsuccessful Query");
+while ($row = mysqli_fetch_assoc($result)) {
+?>
+                <option value="<?php echo $row['cid']; ?>"><?php echo $row['cname']; ?></option>
                 <?php
-               }
-                ?> 
+}
+?> 
             </select>
         </div>
         <div class="form-group">
--- index.php
+++ index.php
@@ -4,14 +4,11 @@ include 'header.php';
 <div id="main-content">
     <h2>All Records We Have</h2>
     <?php
-    $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed");
-
-    $sql = "SELECT * FROM student JOIN studentclass where student.sclass = studentclass.cid";
-    
-   $result = mysqli_query($conn,$sql) or die("Unsuccessful Query");
-
-   if(mysqli_num_rows($result)>0){
-    ?>
+$conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
+$sql = "SELECT * FROM student JOIN studentclass where student.sclass = studentclass.cid";
+$result = mysqli_query($conn, $sql) or die("Unsuccessful Query");
+if (mysqli_num_rows($result) > 0) {
+?>
 
     <table cellpadding="7px">
         <thead>
@@ -24,23 +21,23 @@ include 'header.php';
         </thead>
         <tbody>
             <?php
-            while($row = mysqli_fetch_assoc($result)){
-            ?>
+    while ($row = mysqli_fetch_assoc($result)) {
+?>
 
             <tr>
-                <td><?php echo $row['sid'];?></td>
-                <td><?php echo $row['sname'];?></td>
-                <td><?php echo $row['saddress'];?></td>
-                <td><?php echo $row['cname'];?></td>
-                <td><?php echo $row['sphone'];?></td>
+                <td><?php echo $row['sid']; ?></td>
+                <td><?php echo $row['sname']; ?></td>
+                <td><?php echo $row['saddress']; ?></td>
+                <td><?php echo $row['cname']; ?></td>
+                <td><?php echo $row['sphone']; ?></td>
                 <td>
-                    <a href='edit.php?id=<?php echo $row['sid'];?>'>EDIT</a>
+                    <a href='edit.php?id=<?php echo $row['sid']; ?>'>EDIT</a>
                     <a href='delete-inline.php'>Delete</a>
                 </td>
 
             </tr>
 <?php
- }
+    }
 ?>
         </tbody>
     
